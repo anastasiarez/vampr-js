01cf4996c8789b3b9072b8c81aa72079162053be
Mona Lisa <anastasiia.rn@gmail.com>
2023-08-10 16:36:41 -0400

    commit

--- vampire.js
+++ vampire.js
@@ -9,23 +9,38 @@ class Vampire {
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
-  addOffspring(vampire) {
 
+  addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
 
+    let numberOfVampires = 0;
+    let currentVampire = this;
+
+    // climb "up" the tree (using iteration), counting nodes, until no creator is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
+
+
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
 
+  isMoreSeniorThan(vampire) {
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,6 +51,36 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+      
+      if (this === vampire) {
+        return this;
+      }
+  
+      if (this.creator === null) {
+        return this;
+      }
+  
+      if (vampire.creator === null) {
+        return vampire;
+      }
+  
+      if (this.creator === vampire.creator) {
+        return this.creator;
+      }
+  
+      if (this.creator === vampire) {
+        return vampire;
+      }
+  
+      if (vampire.creator === this) {
+        return this;
+      }
+  
+      if (this.isMoreSeniorThan(vampire)) {
+        return this.creator.closestCommonAncestor(vampire);
+      } else {
+        return vampire.creator.closestCommonAncestor(this);
+      }
 
   }
 }
